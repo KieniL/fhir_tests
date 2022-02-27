f01e2a882c6c3129d7a8084244b873d8249de4aa
Kreidl <lukaskienast0@gmail.com>
2022-02-27 07:54:51 +0100

    added example config to documentation

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,4 +5,6 @@ git clone https://github.com/KieniL/hapi-fhir-jpaserver-starter.git || cd hapi-f
 
 (cd hapi-fhir-jpaserver-starter || true) && docker-compose up -d
 
+docker-compose up -d --force-recreate --no-deps hapi-webserver
+
 docker logs -f hapi-fhir-jpaserver-start
