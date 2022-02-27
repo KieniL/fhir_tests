2a76af63aa6d423327353fb6af1ffbce43e864d2
Kreidl <lukaskienast0@gmail.com>
2022-02-27 18:28:31 +0100

    change to https

--- scripts/enter_dummy_data.sh
+++ scripts/enter_dummy_data.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-BASE_URL="http://localhost:8080/fhir"
+BASE_URL="https://localhost/fhir"
 
 for i in `seq 1 200`; do 
-curl --silent --output /dev/null --show-error --fail\
+curl -k --silent --output /dev/null --show-error --fail\
   -X 'POST' \
   "$BASE_URL/Patient" \
   -H 'accept: application/fhir+json' \
