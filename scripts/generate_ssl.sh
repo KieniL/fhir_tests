8e66c064f0f404b0c4a8f003f380d482d5a61df5
KienastL <lukaskienast0@gmail.com>
2022-04-26 18:27:49 +0200

    added compatibility for GitBash

--- scripts/generate_ssl.sh
+++ scripts/generate_ssl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes   \
+openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 -nodes \
 -keyout hapi-fhir-jpaserver-starter/ssl/localhost.key \
--out hapi-fhir-jpaserver-starter/ssl/localhost.crt -subj '/CN=localhost'   \
+-out hapi-fhir-jpaserver-starter/ssl/localhost.crt -subj '//CN=localhost' \
 -addext 'subjectAltName=DNS:dex.localhost'
